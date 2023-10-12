792e5aaba2c1b3c84fa7a910b975c80925666ceb
Alessandro Coronado Siles <alecoronadosiles@hotmail.com>
2023-10-12 20:37:46 +0000

    Curvapaisses

--- pages/Curva_Paises.py
+++ pages/Curva_Paises.py
@@ -1,22 +1,45 @@
-import io
-import base64
-import pandas as pd
 import streamlit as st
+import pandas as pd
+from streamlit.logger import get_logger
 import altair as alt
+import threading
+import io
 
-def get_table_download_link(df, filename="data.xlsx", text="Download Excel file"):
-    towrite = io.BytesIO()
-    downloaded_file = df.to_excel(towrite, encoding='utf-8', index=False, engine='openpyxl')
-    towrite.seek(0)  
-    b64 = base64.b64encode(towrite.read()).decode()  
-    button_html = f"""<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="{filename}" class="streamlit-btn primary-button">{text}</a>"""
-    return button_html
+LOGGER = get_logger(__name__)
+_lock = threading.Lock()
 
-def process_dataframe(uploaded_file):
-    """Procesa el archivo Excel y devuelve un DataFrame de pandas."""
-    df = pd.read_excel(uploaded_file)
-    # Aquí puedes agregar cualquier otro procesamiento que necesites hacer en el DataFrame.
-    return df
+def process_dataframe(xls_path):
+    with _lock:
+        xls = pd.ExcelFile(xls_path, engine='openpyxl')
+        desembolsos = xls.parse('Desembolsos')
+        operaciones = xls.parse('Operaciones')
+
+    merged_df = pd.merge(desembolsos, operaciones[['IDEtapa', 'FechaVigencia']], on='IDEtapa', how='left')
+    merged_df['FechaEfectiva'] = pd.to_datetime(merged_df['FechaEfectiva'], dayfirst=True)
+    merged_df['FechaVigencia'] = pd.to_datetime(merged_df['FechaVigencia'], dayfirst=True)
+    merged_df['Ano'] = ((merged_df['FechaEfectiva'] - merged_df['FechaVigencia']).dt.days / 366).astype(int)
+    merged_df['Meses'] = ((merged_df['FechaEfectiva'] - merged_df['FechaVigencia']).dt.days / 30).astype(int)
+
+    # Asignar países
+    country_map = {'AR': 'Argentina', 'BO': 'Bolivia', 'BR': 'Brasil', 'PY': 'Paraguay', 'UR': 'Uruguay'}
+    merged_df['Pais'] = merged_df['IDEtapa'].str[:2].map(country_map).fillna('Desconocido')
+
+    result_df = merged_df.groupby(['Pais', 'Ano', 'Meses', 'IDDesembolso'])['Monto'].sum().reset_index()
+    result_df['Monto Acumulado'] = result_df.groupby(['Pais'])['Monto'].cumsum().reset_index(drop=True)
+    result_df['Porcentaje del Monto'] = result_df.groupby(['Pais'])['Monto'].apply(lambda x: x / x.sum() * 100).reset_index(drop=True)
+    result_df['Porcentaje del Monto Acumulado'] = result_df.groupby(['Pais'])['Monto Acumulado'].apply(lambda x: x / x.max() * 100).reset_index(drop=True)
+
+    return result_df
+
+def dataframe_to_excel_bytes(df):
+    """
+    Convert a DataFrame into a BytesIO object containing Excel data.
+    """
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='openpyxl') as writer:
+        df.to_excel(writer, sheet_name='Resultados', index=False)
+    output.seek(0)
+    return output
 
 def run():
     st.set_page_config(
@@ -32,19 +55,28 @@ def run():
     if uploaded_file:
         result_df = process_dataframe(uploaded_file)
         st.write(result_df)
-
-        # Añadir el botón de descarga después de mostrar el dataframe
-        st.markdown(get_table_download_link(result_df), unsafe_allow_html=True)
+        
+        # Permitir la descarga del DataFrame como archivo Excel
+        excel_bytes = dataframe_to_excel_bytes(result_df)
+        st.download_button(
+            label="Descargar resultados como Excel",
+            data=excel_bytes,
+            file_name="resultados_desembolsos.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
 
         selected_country = st.selectbox('Selecciona el País:', result_df['Pais'].unique())
+
         filtered_df = result_df[result_df['Pais'] == selected_country]
 
         df_monto = filtered_df.groupby('Ano')["Monto"].mean().reset_index()
+        df_desembolsos_count = filtered_df.groupby('Ano').size().reset_index(name='Cantidad Desembolsos')
         df_monto_acumulado = filtered_df.groupby('Ano')["Monto Acumulado"].mean().reset_index()
         df_porcentaje_monto_acumulado = filtered_df.groupby('Ano')["Porcentaje del Monto Acumulado"].mean().reset_index()
         df_porcentaje_monto_acumulado["Porcentaje del Monto Acumulado"] = df_porcentaje_monto_acumulado["Porcentaje del Monto Acumulado"].round(2)
 
-        combined_df = pd.concat([df_monto, df_monto_acumulado["Monto Acumulado"], df_porcentaje_monto_acumulado["Porcentaje del Monto Acumulado"]], axis=1)
+        combined_df = pd.merge(df_monto, df_desembolsos_count, on='Ano')
+        combined_df = pd.concat([combined_df, df_monto_acumulado["Monto Acumulado"], df_porcentaje_monto_acumulado["Porcentaje del Monto Acumulado"]], axis=1)
         st.write("Resumen de Datos:")
         st.write(combined_df)
 
@@ -85,4 +117,5 @@ def run():
 
 if __name__ == "__main__":
     run()
+
 
